8e0aafe9e7cc0308d6da21624b7909213134787e
DawnAkuuu <luissilvas12411@gmail.com>
2025-08-15 02:58:42 -0700

    Add activation functions (Sigmoid, ReLU) and binary cross-entropy loss functions, add convolutional layer, add MNIST example

--- NeuralNetworkFramework/activations.py
+++ NeuralNetworkFramework/activations.py
@@ -12,3 +12,26 @@ class Tanh(Activation):
             return 1 - np.tanh(x) ** 2
         
         super().__init__(tanh, tanh_prime)
+
+class Sigmoid(Activation):
+    def __init__(self):
+        
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+        
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s * (1 - s)
+        
+        super().__init__(sigmoid, sigmoid_prime)
+
+class ReLU(Activation):
+    def __init__(self):
+
+        def relu(x):
+            return np.max(0, x)
+        
+        def relu_prime(x):
+            return x > 0
+    
+        super().__init__(relu, relu_prime)
